abdb60c65e4b43fb603c97d7ea88d02f4957fbf2
Stefan Cooper <stefan.cooper27@gmail.com>
2020-07-02 14:34:12 +0100

    Updated styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Zoom from 'react-reveal/Zoom';
 import './App.scss';
 
 class App extends Component {
@@ -9,9 +9,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <p className="title"> S T E F A N <br/> C O O P E R </p>
+      <div className='everything'>
+        <div className="stefancooper">
+          <p className="title"> S T E F A N <br/> C O O P E R </p>
+        </div>
+        <div className="esportshub">
+          <Zoom>
+            <p className='portfolio-1'> ESPORTS HUB </p>
+          </Zoom>
+        </div>
+        <div className="App">
+          <Zoom>
+            <p className='portfolio-2'> PAPER CLICKER </p>
+          </Zoom>
+        </div>
       </div>
+
     );
   }
 }
