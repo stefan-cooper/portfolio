3786be5e97496644ce9e931583603c8c14ed6e9c
Stefan Cooper <stefan.cooper27@gmail.com>
2020-07-02 14:03:25 +0100

    Restyle page and add new fonts

--- src/App.js
+++ src/App.js
@@ -10,20 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p className="title"> S T E F A N <br/> C O O P E R </p>
       </div>
     );
   }
