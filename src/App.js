2306f7cecadf3f75100725c5bb2b204e46c8cbcb
Stefan Cooper <stefan.cooper27@gmail.com>
2020-07-04 14:48:14 +0100

    Cleaned up animations

--- src/App.js
+++ src/App.js
@@ -13,20 +13,23 @@ class App extends Component {
     super(props)
 
     this.state = {
-      p1imagesCount: 1
+      p1imagesCount: 1,
+      fadein1: true
     }
   }
 
-  
-
-  changeImages() {
-    this.setState({p1imagesCount: this.state.p1imagesCount+1})
-    return (
-      <Reveal effect='fadeInUp'>
-        <img className='portfolio-1-images' src={require("./test.png")}></img>
-        <img className='portfolio-1-images' src={require("./test.png")}></img>
-      </Reveal>
-    )
+  changeImages(direction) {
+    if (direction === 'down') {
+      if (this.state.p1imagesCount + 1 < Object.keys(p1images).length)
+        this.setState({p1imagesCount: this.state.p1imagesCount+1, fadein1: true})
+      else
+      this.setState({p1imagesCount: 1, fadein1: true})
+    } else if (direction === 'up') {
+      if (this.state.p1imagesCount > 1)
+        this.setState({p1imagesCount: this.state.p1imagesCount-1, fadein1: true})
+      else
+        this.setState({p1imagesCount: Object.keys(p1images).length-1, fadein1: true})
+    }
   }
 
   render() {
@@ -36,22 +39,26 @@ class App extends Component {
           <p className="title"> S T E F A N <br/> C O O P E R </p>
         </div>
         <div className="esportshub">
-          <Zoom>
             <div className='portfolio-1-text'>
-              <div className='portfolio-1-title'> ESPORTS HUB </div>
-              <div className='portfolio-1-sub'>A website hub for everything university esports</div>
-              <div className='portfolio-1-tert'>REACT - EXPRESS - CLOUDANT - REDUX - ES6 </div>
+              <Zoom left cascade>
+                <div className='portfolio-1-title'>ESPORTS HUB </div>
+                <div className='portfolio-1-sub'>A website hub for everything university esports</div>
+                <div className='portfolio-1-tert'>REACT - EXPRESS - CLOUDANT - REDUX - ES6 </div>
+              </Zoom>
+            </div>
+          <Zoom left cascade>
+            <div onAnimationEnd={() => this.setState({fadein1: false})} className={this.state.fadein1 ? 'portfolio-1-images-div fade-in' : 'portfolio-1-images-div' }>
+              <span onClick={() => this.changeImages('up')} className='center chevron up'></span>
+              <img className='portfolio-1-images' src={p1images["image"+this.state.p1imagesCount]}></img>
+              <img className='portfolio-1-images' src={p1images["image"+(this.state.p1imagesCount+1)]}></img>
+              <span onClick={() => this.changeImages('down')} className='center chevron down'></span>
             </div>
           </Zoom>
-          <div className='portfolio-1-images-div'>
-            <span onClick={() => this.changeImages()} className='center chevron up'></span>
-            <img className='portfolio-1-images fade-in' src={p1images["image"+this.state.p1imagesCount]}></img>
-            <img className='portfolio-1-images fade-in' src={p1images["image"+(this.state.p1imagesCount+1)]}></img>
-            <span className='center chevron down'></span>
-          </div>
+          
+          
         </div>
         <div className="paperclicker">
-          <Zoom>
+          <Zoom right cascade>
             <div className='portfolio-2-title'> PAPER CLICKER </div>
             <div className='portfolio-2-sub'>A clicker game inspired by Universal Paperclips</div>
             <div className='portfolio-2-tert'>REACT - REDUX - ES6 </div>
