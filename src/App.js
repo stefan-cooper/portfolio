d56908f55f76964a2647f59009de2eb44254d385
Stefan Cooper <stefan.cooper27@gmail.com>
2020-07-28 16:27:16 +0100

    Added example text to About Me section and logos for other projects and experience

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import Zoom from 'react-reveal/Zoom';
 import './App.scss';
 
-const p1images = {image1: require("./images/feed1.png"),
-                  image2: require("./images/feed2.png"),
-                  image3: require("./images/schedule1.png"),
-                  image4: require("./images/schedule2.png"),
-                  image5: require("./images/teams1.png"),
-                  image6: require("./images/teams2.png"),
-                  image7: require("./images/teams3.png"),
-                  image8: require("./images/login1.png"),
-                  image9: require("./images/register1.png"),
-                  image10: require("./images/profile1.png"),
-                  image11: require("./images/feed1.png")}
+const p1images = {image1: require('./images/feed1.png'),
+                  image2: require('./images/feed2.png'),
+                  image3: require('./images/schedule1.png'),
+                  image4: require('./images/schedule2.png'),
+                  image5: require('./images/teams1.png'),
+                  image6: require('./images/teams2.png'),
+                  image7: require('./images/teams3.png'),
+                  image8: require('./images/login1.png'),
+                  image9: require('./images/register1.png'),
+                  image10: require('./images/profile1.png'),
+                  image11: require('./images/feed1.png')}
 
-const p2images = {image1: require("./images/earlygame1.png"),
-                  image2: require("./images/earlygame2.png"),
-                  image3: require("./images/earlygame3.png")}
+const p2images = {image1: require('./images/earlygame1.png'),
+                  image2: require('./images/earlygame2.png'),
+                  image3: require('./images/earlygame3.png')}
 
 class App extends Component {
   constructor(props) {
@@ -61,10 +61,10 @@ class App extends Component {
   render() {
     return (
       <div className='everything'>
-        <div className="stefancooper">
-          <p className="title"> S T E F A N <br/> C O O P E R </p>
+        <div className='stefancooper'>
+          <p className='title'> S T E F A N <br/> C O O P E R </p>
         </div>
-        <div className="esportshub">
+        <div className='esportshub'>
           <div className='portfolio-1-text'>
             <Zoom left cascade>
               <div className='portfolio-1-title'>ESPORTS HUB </div>
@@ -75,13 +75,13 @@ class App extends Component {
           <Zoom left cascade>
             <div onAnimationEnd={() => this.setState({fadein1: false})} className={this.state.fadein1 ? 'portfolio-1-images-div fade-in' : 'portfolio-1-images-div' }>
               <span onClick={() => this.changeImagesEsportsHub('up')} className='center chevron up'></span>
-              <img alt='Examplar Esports Hub 1' className='portfolio-1-images' src={p1images["image"+this.state.p1imagesCount]}></img>
-              <img alt='Examplar Esports Hub 2' className='portfolio-1-images' src={p1images["image"+(this.state.p1imagesCount+1)]}></img>
+              <img alt='Examplar Esports Hub 1' className='portfolio-1-images' src={p1images['image'+this.state.p1imagesCount]}></img>
+              <img alt='Examplar Esports Hub 2' className='portfolio-1-images' src={p1images['image'+(this.state.p1imagesCount+1)]}></img>
               <span onClick={() => this.changeImagesEsportsHub('down')} className='center chevron down'></span>
             </div>
           </Zoom>
         </div>
-        <div className="paperclicker">
+        <div className='paperclicker'>
           <div className='portfolio-2-text'>
             <Zoom right cascade>
               <div className='portfolio-2-title'>PAPERCLICKER</div>
@@ -92,25 +92,52 @@ class App extends Component {
           <Zoom right cascade>
             <div onAnimationEnd={() => this.setState({fadein2: false})} className={this.state.fadein2 ? 'portfolio-2-images-div fade-in' : 'portfolio-2-images-div' }>
               <span onClick={() => this.changeImagesPaperClicker('up')} className='center chevron up'></span>
-              <img alt='Examplar Paper Clicker 1' className='portfolio-1-images' src={p2images["image"+this.state.p2imagesCount]}></img>
-              <img alt='Examplar Paper Clicker 2' className='portfolio-1-images' src={p2images["image"+(this.state.p2imagesCount+1)]}></img>
+              <img alt='Examplar Paper Clicker 1' className='portfolio-1-images' src={p2images['image'+this.state.p2imagesCount]}></img>
+              <img alt='Examplar Paper Clicker 2' className='portfolio-1-images' src={p2images['image'+(this.state.p2imagesCount+1)]}></img>
               <span onClick={() => this.changeImagesPaperClicker('down')} className='center chevron down'></span>
             </div>
           </Zoom>
         </div>
         <div className='otherProjects'>
           <div className='portfolio-3-title'> OTHER PROJECTS & EXPERIENCE </div>
+          <div className='otherProjects-collection'>
+            <div className='otherProjects-ibm'>
+              <img alt='IBM Logo' className='ibm' src={require('./images/ibm.png')}></img>
+              <div className='otherProjects-content'>
+                Daeja ViewOne<br/>
+                Early Professionals Hub
+                
+              </div>
+            </div>
+            <div className='otherProjects-uh'>
+              <img alt='UH Logo' className='uh' src={require('./images/uh.png')}></img>
+              <div className='otherProjects-content'>
+                Lorem<br/>
+                Ipsum
+              </div>
+            </div>
+            <div className='otherProjects-hertsgg'>
+              <img alt='hertsgg Logo' className='hertsgg' src={require('./images/hertsgg.png')}></img>
+              <div className='otherProjects-content'>
+                Lorem<br/>
+                Ipsum
+              </div>
+            </div>
+          </div>
         </div>
         <div className='about'>
           <div className='pic-content'>
-            <img className='pic' src={require('./images/about.png')}></img>
+            <img alt='My face!' className='pic' src={require('./images/about.png')}></img>
           </div>
           <div className='about-content'>
             <div className='about-title'> ABOUT ME </div>
-            <div>
-              <a href={'https://github.com/3eak'}><img className='github' src={require('./images/github.png')}></img></a>
-              <a href={'https://twitter.com/3eakStefan'}><img className='twitter' src={require('./images/twitter.png')}></img></a>
-              <a href={'https://www.linkedin.com/in/stefan-cooper-491355171/'}><img className='twitter' src={require('./images/li.png')}></img></a>
+            <div className='about-text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ante arcu. Ut vel augue nec tortor tempus tempus sed vitae dolor. Suspendisse at tincidunt metus. Sed sed blandit felis. Integer eu urna vel dolor luctus tempor. Nulla in vestibulum nunc. Integer pellentesque auctor ante, vel dictum sem accumsan non. Pellentesque dapibus erat velit, sed fringilla mauris placerat vitae. In hac habitasse platea dictumst. Praesent sed nulla nec ante pharetra dignissim.
+            </div>
+            <div className='urls'>
+              <a target='_blank' rel='noopener noreferrer' href={'https://github.com/3eak'}><img alt='GitHub Logo' className='github' src={require('./images/github.png')}></img></a>
+              <a target='_blank' rel='noopener noreferrer' href={'https://twitter.com/3eakStefan'}><img alt='Twitter Logo' className='twitter' src={require('./images/twitter.png')}></img></a>
+              <a target='_blank' rel='noopener noreferrer' href={'https://www.linkedin.com/in/stefan-cooper-491355171/'}><img alt='Linkedin Logo' className='twitter' src={require('./images/li.png')}></img></a>
             </div>
 
           </div>
