07799822befaff0515fb1e7f505164ebc3655015
Stefan Cooper <stefan.cooper27@gmail.com>
2020-08-02 14:24:37 +0100

    Refactored portfolio content into Portfolios.js and added moveable extra portfolios section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Zoom from 'react-reveal/Zoom';
 import './App.scss';
+import { ibm, uh, hertsgg, test, esportshub, paperclicker } from './Portfolios';
 
 const p1images = {image1: require('./images/feed1.png'),
                   image2: require('./images/feed2.png'),
@@ -18,6 +19,12 @@ const p2images = {image1: require('./images/earlygame1.png'),
                   image2: require('./images/earlygame2.png'),
                   image3: require('./images/earlygame3.png')}
 
+
+const extraportfolios = {portfolio1: ibm(),
+  portfolio2: uh(),
+  portfolio3: hertsgg(),
+  portfolio4: test()}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -25,6 +32,7 @@ class App extends Component {
     this.state = {
       p1imagesCount: 1,
       p2imagesCount: 1,
+      p3Count: 1,
       fadein1: true,
       fadein2: true
     }
@@ -58,6 +66,20 @@ class App extends Component {
     }
   }
 
+  changePortfolio(direction) {
+    if (direction === 'right') {
+      if (this.state.p3Count - 1 === 0)
+        this.setState({p3Count: 4})
+      else
+        this.setState({p3Count: this.state.p3Count - 1})
+    } else if (direction === 'left') {
+      if (this.state.p3Count + 1 > Object.keys(extraportfolios).length)
+        this.setState({p3Count: 1})
+      else
+        this.setState({p3Count: this.state.p3Count + 1})
+      }
+  }
+
   render() {
     return (
       <div className='everything'>
@@ -65,17 +87,9 @@ class App extends Component {
           <p className='title'> S T E F A N <br/> C O O P E R </p>
         </div>
         <div className='esportshub'>
-          <div className='portfolio-1-text'>
-            <Zoom left cascade>
-              <div className='portfolio-1-title'>ESPORTS HUB </div>
-              <div className='portfolio-1-sub'>A website hub for everything university esports</div>
-              <div className='portfolio-1-tert'>REACT - EXPRESS - CLOUDANT - REDUX - ES6 </div>
-              <div className='portfolio-1-links'>
-                <a className='portfolio-1-links-pics' target='_blank' rel='noopener noreferrer' href={'https://github.com/3eak/esportshub-server'}><img alt='GitHub Logo' className='github' src={require('./images/github.png')}></img></a>
-                <a className='portfolio-1-links-pics' target='_blank' rel='noopener noreferrer' href={'https://hub.stefancooper.com'}><img alt='Web Logo' className='github' src={require('./images/web.png')}></img></a>
-              </div>
+        <Zoom left cascade>
+            {esportshub()}
             </Zoom>
-          </div>
           <Zoom left cascade>
             <div onAnimationEnd={() => this.setState({fadein1: false})} className={this.state.fadein1 ? 'portfolio-1-images-div fade-in' : 'portfolio-1-images-div' }>
               <span onClick={() => this.changeImagesEsportsHub('up')} className='center chevron up'></span>
@@ -86,17 +100,9 @@ class App extends Component {
           </Zoom>
         </div>
         <div className='paperclicker'>
-          <div className='portfolio-2-text'>
-            <Zoom right cascade>
-              <div className='portfolio-2-title'>PAPERCLICKER</div>
-              <div className='portfolio-2-sub'>A clicker game inspired by Universal Paperclips</div>
-              <div className='portfolio-2-tert'>REACT - REDUX - ES6 </div>
-              <div className='portfolio-2-links'>
-                <a className='portfolio-2-links-pics' target='_blank' rel='noopener noreferrer' href={'http://clicker.stefancooper.com'}><img alt='Web Logo' className='github' src={require('./images/web.png')}></img></a>
-                <a className='portfolio-2-links-pics' target='_blank' rel='noopener noreferrer' href={'https://github.com/3eak/paper-clicker'}><img alt='GitHub Logo' className='github' src={require('./images/github.png')}></img></a>
-              </div>
-            </Zoom>
-          </div>
+          <Zoom right cascade>
+            {paperclicker()}
+          </Zoom>
           <Zoom right cascade>
             <div onAnimationEnd={() => this.setState({fadein2: false})} className={this.state.fadein2 ? 'portfolio-2-images-div fade-in' : 'portfolio-2-images-div' }>
               <span onClick={() => this.changeImagesPaperClicker('up')} className='center chevron up'></span>
@@ -109,33 +115,11 @@ class App extends Component {
         <div className='otherProjects'>
           <div className='otherProjects-title'> OTHER PROJECTS & EXPERIENCE </div>
           <div className='otherProjects-collection'>
-            <div className='otherProjects-ibm'>
-              <img alt='IBM Logo' className='ibm' src={require('./images/ibm.png')}></img>
-              <div className='otherProjects-content'><br />
-                <p className='otherProjects-content-title'>Daeja ViewONE Virtual</p>
-                Working as a developer here I have been a part of an agile development cycle. Developing in a variety of languages from XML to Java. Whilst working in the Daeja team I have worked on many different projects, furthering my knowledge of automated testing with technologies (Vagrant, Selenium, REST tests). As well as working in a complex and large codebase that has been maintained for a long time.<br/>
-                <p className='otherProjects-content-title'>Early Professionals Hub</p>
-                Whilst at IBM I worked on a giveback team of interns on the EPH (Early Professionals Hub).A web-based project written in React using a variety of web based libraries such as Redux. The site interacts with a cloud based database (IBM Cloudant) to maintain information on its users and various data tracking.
-              </div>
-            </div>
-            <div className='otherProjects-uh'>
-              <img alt='UH Logo' className='uh' src={require('./images/uh.png')}></img>
-              <div className='otherProjects-content'><br />
-                <p className='otherProjects-content-title'>Computer Science (Software Engineering) (2016-2020)</p>
-                I have grown a lot throughout my time at university, achieving a First Class with Honours averaging an 84% across my modules. Notable module grades include Software Engineering Practice (94%), Mobile Computing (85%) and Software Engineering Project (84%). Furthermore, I have interacted with many extra-curricular activities such as working on the Esports Society and being a voting member on the Student Council.
-                <p className='otherProjects-content-title'>Projects and Experiences</p>
-                At university, I was given the opportunity to work on a variety of different projects to expand my skillset. In Mobile Computing I learned all about developing on Apple products, learning Swift to make a small iOS game. In Software Engineering Practice and Object-Oriented Programming I furthered my skills and understanding of object-oriented programming in Java.
-              </div>
-            </div>
-            <div className='otherProjects-hertsgg'>
-              <img alt='hertsgg Logo' className='hertsgg' src={require('./images/hertsgg.png')}></img>
-              <div className='otherProjects-content'>
-                <p className='otherProjects-content-title'>Chair for Esports Society (2019-2020)</p>
-                I am responsible for managing the committee and ensuring the society continues to thrive as one of the premier esports societies in the country. Including maintaining and holding talks with our associated sponsors including Overclockers UK and ASUS ROG.
-                <p className='otherProjects-content-title'>Treasurer for Esports Society (2017-2019)</p>
-                I am responsible for the outgoing transactions and understand the financing of the society. This role further included communicating effectively with a range of other volunteers in the society and to deal with a variety of customers who join the society.
-              </div>
-            </div>
+          <span onClick={() => this.changePortfolio('left')} className='center chevron left'></span>
+            {extraportfolios['portfolio'+this.state.p3Count]}
+            {this.state.p3Count + 1 > Object.keys(extraportfolios).length ? extraportfolios['portfolio1'] : extraportfolios['portfolio'+(this.state.p3Count+1)]}
+            {this.state.p3Count + 2 > Object.keys(extraportfolios).length ? extraportfolios['portfolio'+(this.state.p3Count + 2 - Object.keys(extraportfolios).length)] : extraportfolios['portfolio'+(this.state.p3Count+2)]}
+            <span onClick={() => this.changePortfolio('right')} className='center chevron right'></span>
           </div>
         </div>
         <div className='about'>
